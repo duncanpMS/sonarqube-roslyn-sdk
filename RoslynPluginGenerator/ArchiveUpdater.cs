c680a2d34a33a67614c79b646558e2f3f884f822
duncanp <duncanp@microsoft.com>
2016-03-01 18:41:35 +0000

    [SFSRAP-37] WIP

---
+++ RoslynPluginGenerator/ArchiveUpdater.cs
@@ -0,0 +1,97 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ArchiveUpdater.cs" company="SonarSource SA and Microsoft Corporation">
+//   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
+//   Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using SonarQube.Plugins.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace SonarQube.Plugins.Roslyn
+{
+    /// <summary>
+    /// Updates an existing archive (zip, jar) by inserting additional files
+    /// </summary>
+    public class ArchiveUpdater
+    {
+        private readonly string workingDirectory;
+        private readonly IDictionary<string, string> fileMap;
+        private readonly ILogger logger;
+
+        private string inputArchiveFilePath;
+        private string outputArchiveFilePath;
+
+        #region Public methods
+
+        public ArchiveUpdater(string workingDirectory, ILogger logger)
+        {
+            if (string.IsNullOrWhiteSpace(workingDirectory))
+            {
+                throw new ArgumentNullException("workingDirectory");
+            }
+            if (logger == null)
+            {
+                throw new ArgumentNullException("logger");
+            }
+            this.logger = logger;
+            this.workingDirectory = workingDirectory;
+
+            this.fileMap = new Dictionary<string, string>();
+        }
+
+        public ArchiveUpdater SetInputArchive(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException("filePath");
+            }
+            this.inputArchiveFilePath = filePath;
+            return this;
+        }
+
+        public ArchiveUpdater SetOutputArchive(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException("filePath");
+            }
+            this.outputArchiveFilePath = filePath;
+            return this;
+        }
+
+        public ArchiveUpdater AddFile(string sourceFilePath, string relativeTargetFilePath)
+        {
+            this.fileMap[relativeTargetFilePath] = sourceFilePath;
+            return this;
+        }
+
+        public void UpdateArchive()
+        {
+            string unpackedDir = Utilities.CreateSubDirectory(this.workingDirectory, "unpacked");
+            ZipFile.ExtractToDirectory(this.inputArchiveFilePath, unpackedDir);
+
+            // Add in the new files
+            foreach (KeyValuePair<string, string> kvp in this.fileMap)
+            {
+                string targetFilePath = Path.Combine(unpackedDir, kvp.Key);
+
+                Directory.CreateDirectory(Path.GetDirectoryName(targetFilePath));
+                File.Copy(kvp.Value, targetFilePath);
+            }
+
+            // Re-zip
+            if (string.IsNullOrWhiteSpace(this.outputArchiveFilePath))
+            {
+                this.outputArchiveFilePath = this.inputArchiveFilePath;
+            }
+
+            ZipFile.CreateFromDirectory(unpackedDir, this.outputArchiveFilePath);
+        }
+
+        #endregion
+    }
+}
---
+++ RoslynPluginGenerator/RoslynPluginJarBuilder.cs
@@ -0,0 +1,105 @@
+﻿using SonarQube.Plugins.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SonarQube.Plugins.Roslyn
+{
+    public class RoslynPluginJarBuilder
+    {
+        private const string EmptyTemplateJarResourceName = "SonarQube.Plugins.Roslyn.Resources.sonar-roslyn-sdk-template-plugin-1.0-empty.jar";
+
+        public const string ManifestFileName = "MANIFEST.MF";
+        public const string RelativeManifestResourcePath = "META-INF\\" + ManifestFileName;
+
+
+        private readonly ILogger logger;
+
+        #region Public methods
+
+        public RoslynPluginJarBuilder(ILogger logger)
+        {
+            if (logger == null)
+            {
+                throw new ArgumentNullException("logger");
+            }
+            this.logger = logger;
+        }
+
+        public void BuildJar(RoslynPluginDefinition definition, string workingDirectory, string outputFilePath)
+        {
+            if (definition == null)
+            {
+                throw new ArgumentNullException("definition");
+            }
+            if (string.IsNullOrWhiteSpace(workingDirectory))
+            {
+                throw new ArgumentNullException("workingDirectory");
+            }
+            if (string.IsNullOrWhiteSpace(outputFilePath))
+            {
+                throw new ArgumentNullException("outputFilePath");
+            }
+
+            // Create the config and manifest files
+            string configFilePath = BuildConfigFile(definition, workingDirectory);
+
+            string manifestFilePath = Path.Combine(workingDirectory, ManifestFileName);
+            definition.Manifest.Save(manifestFilePath);
+
+            // Update the jar
+            string templateJarFilePath = ExtractTemplateJarFile(workingDirectory);
+            ArchiveUpdater updater = new ArchiveUpdater(workingDirectory, this.logger);
+
+            updater.SetInputArchive(templateJarFilePath)
+                .SetOutputArchive(outputFilePath)
+                .AddFile(manifestFilePath, RelativeManifestResourcePath)
+                .AddFile(configFilePath, "config.xml")
+                .AddFile(definition.RulesFilePath, "rules.xml")
+                .AddFile(definition.StaticResourceName, definition.SourceZipFilePath);
+
+            if (!string.IsNullOrWhiteSpace(definition.SqaleFilePath))
+            {
+                updater.AddFile(definition.SqaleFilePath, "sqale.xml");
+            }
+
+            updater.UpdateArchive();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static string ExtractTemplateJarFile(string workingDirectory)
+        {
+            string templateJarFilePath = Path.Combine(workingDirectory, "template.jar");
+
+            using (Stream resourceStream = typeof(RoslynPluginJarBuilder).Assembly.GetManifestResourceStream(EmptyTemplateJarResourceName))
+            {
+                using (FileStream file = new FileStream(templateJarFilePath, FileMode.CreateNew, FileAccess.Write))
+                {
+                    resourceStream.CopyTo(file);
+                    file.Flush();
+                }
+            }
+
+            return templateJarFilePath;
+        }
+
+        private string BuildConfigFile(RoslynPluginDefinition definition, string workingDirectory)
+        {
+            string configFilePath = Path.Combine(workingDirectory, "config.xml");
+
+            RoslynSdkConfiguration config = new RoslynSdkConfiguration();
+
+            // TODO:
+            config.Save(configFilePath);
+            return configFilePath;
+        }
+
+        #endregion
+
+    }
+}
